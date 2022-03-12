4e912459c6f49c8dc97c26dbf9d394279a947610
Vicko Danendra <vickodanendra@gmail.com>
2022-03-12 15:41:04 +0700

    Mengubah Halaman 2

--- lib/another_page.dart
+++ lib/another_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 class AnotherPage extends StatefulWidget {
   const AnotherPage({Key? key}) : super(key: key);
@@ -11,68 +12,33 @@ class AnotherPage extends StatefulWidget {
 class _AnotherPageState extends State<AnotherPage> {
   int angka = 0;
 
-  void tambah(){
-    setState(() {
-      angka++;
-    });
-  }
+  final _suggestions = <WordPair>[];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("VickoApp"),
-        centerTitle: true,
-        backgroundColor: Colors.purple,
+        title: const Text('Startup Name Generator'),
       ),
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.only(left: 16, right: 16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                  angka.toString(),
-                  style: TextStyle(fontSize: 50, color: Colors.purple)),
-              Row(
-                children: [
-                  Expanded(
-                    child: ElevatedButton(onPressed: (){
-                      tambah();
-                    }, child: Text("Tambah")),
-                  ),
-                  SizedBox(width: 16),
-                  Expanded(
-                    child: ElevatedButton(onPressed: (){
-                      setState(() {
-                        angka--;
-                      });
-                    }, child: Text("Kurang")),
-                  ),
-                ],),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return const Divider(); /*2*/
 
-              Row(
-                children: [
-                  Expanded(
-                    child: ElevatedButton(onPressed: (){
-                      setState(() {
-                        angka = 0;
-                      });
-                    }, child: Text("Reset"),
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.red,
-                      ),
-                    ),
-                  ),
-                ],
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return ListTile(
+            title: Center(
+              child: Text(
+                _suggestions[index].asPascalCase,
+                style: TextStyle(fontWeight: FontWeight.bold),
               ),
-            ],
-          ),),
+            ),
+          );
+        },
       ),
-      // floatingActionButton: FloatingActionButton(onPressed: () {
-      //   setState(() {
-      //     angka*=2;
-      //   });
-      // },),
     );
   }
 }
